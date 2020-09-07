2770683d3b13b2b4d8bc87fe4bf5d5f5f798016e
KuroCoder <habib.pa98@gmail.com>
2020-09-07 16:21:52 +0430

    Add Controller Order

--- SadafStore.Core/Services/Interfaces/IOrderService.cs
+++ SadafStore.Core/Services/Interfaces/IOrderService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SadafStore.DataLayer.Entities.Order;
 
 namespace SadafStore.Core.Services.Interfaces
 {
@@ -8,5 +9,6 @@ namespace SadafStore.Core.Services.Interfaces
     {
         int AddOrder(string userName, int productId);
         void UpdatePriceOrder(int orderId);
+        Order GetOrderForUserPanel(string userName, int userId);
     }
 }
---
+++ SadafStore.web/Areas/UserPanel/Controllers/MyOrdersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SadafStore.Core.Services.Interfaces;
+
+namespace SadafStore.web.Areas.UserPanel.Controllers
+{
+    [Authorize]
+    [Area("UserPanel")]
+    public class MyOrdersController : Controller
+    {
+        private IOrderService _orderService;
+
+        public MyOrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
